3a30be563f5c65f3cab463ca06ba783a78b9d7db
jh.ko <jeonghunko@gmail.com>
2024-07-03 11:10:36 +0900

    [feature] p37

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -9,11 +9,15 @@ public:
 	
 };
 
-TEST(PrimeFactors, TestName) {
+TEST(PrimeFactors, Of1) {
 	PrimeFactor primeFactor;
 	vector<int> expected = {};
 	EXPECT_EQ(expected, primeFactor.of(1));
 }
 
-
+TEST(PrimeFactors, Of2) {
+	PrimeFactor primeFactor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, primeFactor.of(2));
+}
 
