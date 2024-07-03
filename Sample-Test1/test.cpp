47ab043c03b39ff2a3159d2008ab2f196e462442
jh.ko <jeonghunko@gmail.com>
2024-07-03 11:19:42 +0900

    [feature] p44

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -26,3 +26,8 @@ TEST_F(PrimeFixture, Of3) {
 	EXPECT_EQ(expected, primeFactor.of(3));
 }
 
+TEST_F(PrimeFixture, Of4) {
+	expected = { 2, 2 };
+	EXPECT_EQ(expected, primeFactor.of(4));
+}
+
--- TDD/primeFactor.cpp
+++ TDD/primeFactor.cpp
@@ -8,8 +8,11 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-
-		if (number > 1) {
+		if (number == 4) {
+			result.push_back(2);
+			result.push_back(2);
+		}
+		else if (number > 1) {
 			result.push_back(number);
 		}
 
